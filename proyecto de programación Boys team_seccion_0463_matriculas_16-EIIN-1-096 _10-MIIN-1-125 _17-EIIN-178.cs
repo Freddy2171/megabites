ade3275e12063bd952fbaf7a45e85d870322b864
Freddy2171 <33640640+Freddy2171@users.noreply.github.com>
2017-12-07 19:35:02 -0400

    Add files via upload

---
+++ proyecto de programación Boys team_seccion_0463_matriculas_16-EIIN-1-096 _10-MIIN-1-125 _17-EIIN-178.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string linea;
+            Console.Write("Ingrese Multiplicacdor: ");
+            linea = Console.ReadLine();
+            n = int.Parse(linea);
+
+            for(int i=1; i<=15; i++)
+            {
+                Console.Write(i + " x " + n + " = " + i * n + "\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
